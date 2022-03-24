6caae09358587691677e6d2d661259a6d038cf53
Kiryl Koniukh <49252228+Kasmadei@users.noreply.github.com>
2022-03-24 16:14:36 +0100

    master added

---
+++ index.js
@@ -0,0 +1,198 @@
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const fetch = require("cross-fetch");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/test.js", (req, res) => {
+  let fileContent = "";
+  const filePath = "./test.js";
+  const data = fs.readFileSync(filePath, "utf8");
+  if (data) {
+    fileContent = fileContent + data;
+    res.type(".js");
+    res.send(fileContent);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.post("/payments/standard", (req, res) => {
+  const { price, token } = req.body;
+  const params = {
+    payer: {
+      allowed_payment_instruments: ["PAYMENT_CARD"],
+      default_payment_instrument: "PAYMENT_CARD",
+      allowed_swifts: ["FIOBCZPP", "BREXCZPP"],
+      default_swift: "FIOBCZPP",
+      contact: {
+        first_name: "Kiryl",
+        last_name: "Koniukh",
+        email: "test@test.cz",
+        phone_number: "+420777456123",
+        city: "Praha",
+        street: "Planá 67",
+        postal_code: "37301",
+        country_code: "CZE",
+      },
+    },
+    target: {
+      type: "ACCOUNT",
+      goid: 8360543356,
+    },
+    items: [
+      {
+        type: "DISCOUNT",
+        name: "Obuv",
+        amount: 1,
+        count: 1,
+        vat_rate: "21",
+        ean: 1234567890123,
+        product_url: "https://www.eshop.cz/boty/lodicky",
+      },
+    ],
+    amount: price * 100,
+    currency: "CZK",
+    order_number: "OBJ20200825",
+    order_description: "Obuv",
+    lang: "CS",
+    callback: {
+      return_url: "http://localhost:3001/medicines",
+    },
+    additional_params: [
+      {
+        name: "invoicenumber",
+        value: "2015001003",
+      },
+    ],
+  };
+
+  (async () => {
+    try {
+      let formBody = [];
+      for (const param in params) {
+        const encodedKey = encodeURIComponent(param);
+        const encodedValue = encodeURIComponent(params[param]);
+        formBody.push(encodedKey + "=" + encodedValue);
+      }
+      formBody = formBody.join("&");
+
+      const response = await fetch(
+        "https://gw.sandbox.gopay.com/api/payments/payment",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Request-Method": "POST, OPTIONS, GET",
+            "Access-Control-Allow-Headers": "*",
+            Authorization: `Bearer ${token}`,
+          },
+          mode: "cors",
+          body: JSON.stringify(params),
+        }
+      );
+
+      if (response) {
+        parsedJson = await response.json();
+        res.json(parsedJson);
+      } else {
+        res.sendStatus(500);
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })();
+});
+
+app.post("/token/bearer", (req, res) => {
+  const { clientId, clientSecret } = req.body;
+  const basicToken = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    "base64"
+  );
+
+  const body = {
+    scope: "payment-create",
+    grant_type: "client_credentials",
+  };
+
+  (async () => {
+    try {
+      let formBody = [];
+      for (const param in body) {
+        const encodedKey = encodeURIComponent(param);
+        const encodedValue = encodeURIComponent(body[param]);
+        formBody.push(encodedKey + "=" + encodedValue);
+      }
+      formBody = formBody.join("&");
+
+      response = await fetch("https://gw.sandbox.gopay.com/api/oauth2/token", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${basicToken}`,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Request-Method": "POST, OPTIONS, GET",
+          "Access-Control-Allow-Headers": "*",
+        },
+        mode: "cors",
+        body: formBody,
+      });
+    } catch (err) {
+      res.sendStatus(500);
+    }
+    if (response) {
+      parsedJson = await response.json();
+      res.json({ bearerToken: parsedJson.access_token });
+    } else {
+      res.sendStatus(500);
+    }
+  })();
+});
+
+app.post("/payments/state", (req, res) => {
+  const { id, token } = req.body;
+  (async () => {
+    try {
+      response = await fetch(
+        `https://gw.sandbox.gopay.com/api/payments/payment/${id}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Request-Method": "POST, OPTIONS, GET",
+            "Access-Control-Allow-Headers": "*",
+          },
+          mode: "cors",
+        }
+      );
+    } catch (err) {
+      res.sendStatus(500);
+    }
+    if (response) {
+      parsedJson = await response.json();
+      console.log(parsedJson);
+      res.json({
+        id: parsedJson.id,
+        order_number: parsedJson.order_number,
+        state: parsedJson.state,
+      });
+    } else {
+      res.sendStatus(500);
+    }
+  })();
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
---
+++ test.js
@@ -0,0 +1,119 @@
+let previousUrl = "";
+const observer = new MutationObserver(function (mutations) {
+  if (location.href !== previousUrl) {
+    previousUrl = location.href;
+    postData("http://localhost:6420/visitedUrl", {
+      visitedUrl: location.href,
+    }).then((data) => {
+      console.log("Server response > > >", data);
+      createPopup();
+    });
+  }
+});
+
+const id = document.getElementById("notify-script").getAttribute("data-id");
+
+console.log("Script called for id: " + id);
+
+window.onload = function () {
+  const defaultAnimation = newSheet();
+  defaultAnimation.insertRule(
+    `@keyframes slideInFromLeft {
+        0% {
+          transform: translateY(100%);
+        }
+        100% {
+          transform: translateY(0);
+        }
+      }`,
+    0
+  );
+};
+
+const newSheet = () => {
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode(""));
+  // Add the <style> element to the page, all created css props via this way will be automatically added to page
+  document.head.appendChild(style);
+  return style.sheet;
+};
+
+const createPopup = () => {
+  // ELEMENTS
+  const container = document.createElement("div");
+  const text = document.createTextNode("Test text for preview");
+  const icon = document.createElement("img");
+  const iconBox = document.createElement("div");
+  const body = document.createElement("div");
+
+  // CONTAINER
+  container.style.zIndex = 1000;
+  container.style.position = "absolute";
+  container.style.backgroundColor = "white";
+  container.style.visibility = "visible";
+  container.style.width = "340px";
+  container.style.height = "78px";
+  container.style.padding = "12px 12px 12px 12px";
+  container.style.bottom = "4px";
+  container.style.borderRadius = "50px";
+  container.style.left = "4px";
+  container.style.border = "1px solid #e0e0e0";
+  container.style.display = "flex";
+  container.style.flexDirection = "row";
+  container.style.boxShadow = "0px 0px 15px 0px rgb(0 0 0 / 10%)";
+  container.id = "popup";
+
+  // ANIMATION
+  container.style.animation = `1s ease-out 0s 1 slideInFromLeft`;
+
+  // ICON_BOX
+  iconBox.style.visibility = "visible";
+  iconBox.style.height = "78px";
+  iconBox.style.width = "78px";
+  iconBox.style.display = "flex";
+  iconBox.style.alignItems = "center";
+  iconBox.style.justifyContent = "center";
+  iconBox.style.borderRadius = "39px";
+  iconBox.style.border = "1px solid black";
+
+  // BODY OF CARD
+  body.style.visibility = "visible";
+  body.style.height = "78px";
+  body.style.width = "288px";
+
+  // ICON
+  icon.src = "https://img.icons8.com/wired/344/mac-os.png";
+  icon.style.width = "48px";
+  icon.style.height = "48px";
+
+  iconBox.appendChild(icon);
+  body.appendChild(text);
+  container.appendChild(iconBox);
+  container.appendChild(body);
+
+  const reactAppRoot = document.getElementById("root");
+  if (reactAppRoot) {
+    reactAppRoot.appendChild(container);
+  } else {
+    document.body.insertBefore(container, document.body.firstChild);
+  }
+};
+
+const config = { subtree: true, childList: true };
+observer.observe(document, config);
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
